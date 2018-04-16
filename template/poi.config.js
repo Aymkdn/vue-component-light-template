c609528f86614cf102689e39d04f7c25bfd7e580
Aymeric <aymeric@kodono.info>
2018-04-16 14:28:25 +0200

    add HTML template for build

--- template/poi.config.js
+++ template/poi.config.js
@@ -1,9 +1,13 @@
-// optional config file for poi
-/*const webpack = require.main.require('webpack')
+// config file for poi
+const webpack = require.main.require('webpack')
 
 module.exports = {
   webpack:function(config) {
-    config.plugins.push(new webpack.IgnorePlugin(/^sp-request$/))
+    //config.plugins.push(new webpack.IgnorePlugin(/^sp-request$/))
+  },
+  html:{
+    template:"./src/build.html",
+    minify:false,
+    inject:'head'
   }
 }
-*/
