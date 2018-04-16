fc1a1409beaf36bf0a9624cb335dc6aa1726b388
Aymeric <aymeric@kodono.info>
2018-04-16 14:44:28 +0200

    fix poi config for demo

--- template/poi.config.js
+++ template/poi.config.js
@@ -1,13 +1,18 @@
 // config file for poi
-const webpack = require.main.require('webpack')
+const webpack = require.main.require('webpack');
+const demo = process.env.NODE_ENV === 'development' || process.env.npm_lifecycle_event === 'demo';
 
-module.exports = {
+let options = {
   webpack:function(config) {
     //config.plugins.push(new webpack.IgnorePlugin(/^sp-request$/))
-  },
-  html:{
+  }
+}
+if (!demo) {
+  options.html={
     template:"./src/build.html",
     minify:false,
     inject:'head'
   }
-}
+};
+
+module.exports = options;
